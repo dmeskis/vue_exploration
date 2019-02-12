3cf4a22769425c22b26cb750c7c9a9edb3edf9b9
Mesky <dmeskis@gmail.com>
2019-02-12 10:33:20 -0700

    Learning interpolation squiggly braces and v-directives

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Skills from '@/components/Skills'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Skills',
+      component: Skills
     }
   ]
 })
