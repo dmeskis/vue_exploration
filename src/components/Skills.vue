5423c700e6daa20d63139dde0dad778a91ec229c
Mesky <dmeskis@gmail.com>
2019-02-12 11:26:36 -0700

    Add VeeValidate. Only submit skill on valid user input

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,7 +3,8 @@
     <div class='holder'>
 
       <form @submit.prevent="addSkill">
-        <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+        <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
       </form>
 
       <ul>
@@ -28,8 +29,14 @@ export default {
   },
   methods: {
     addSkill () {
-      this.skills.push({skill: this.skill})
-      this.skill = ''
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = ''
+        } else {
+          console.log('Not valid')
+        }
+      })
     }
   }
 }
