77e4ca9a9e65f7d9b600a1a08d2a5e0aef846e6c
Mesky <dmeskis@gmail.com>
2019-02-12 11:47:13 -0700

    Create About component and route to about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Skills from '@/components/Skills'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Skills',
+      name: 'skills',
       component: Skills
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     }
   ]
 })
